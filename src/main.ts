c7045082106677703701ef9d4155e575c43c5f22
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-01-18 18:11:18 +0200

    Add texture

--- src/main.ts
+++ src/main.ts
@@ -21,16 +21,26 @@ function main() {
 
   const vsSource = `
     attribute vec4 aVertexPosition;
+    attribute vec2 aTextureCoord;
+
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
-    void main() {
+
+    varying highp vec2 vTextureCoord;
+
+    void main(void) {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vTextureCoord = aTextureCoord;
     }
   `;
 
   const fsSource = `
-    void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    varying highp vec2 vTextureCoord;
+
+    uniform sampler2D uSampler;
+
+    void main(void) {
+      gl_FragColor = texture2D(uSampler, vTextureCoord);
     }
   `;
 
@@ -40,16 +50,21 @@ function main() {
     program: shaderProgram,
     attribLocations: {
       vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+      textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
     },
     uniformLocations: {
       projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
       modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+      uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
     },
   };
 
   const buffers = initBuffers(gl);
 
-  drawScene(gl, programInfo, buffers);
+  const texture = loadTexture(gl, "card_back.png");
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+  drawScene(gl, programInfo, buffers, texture);
 }
 
 function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
@@ -100,12 +115,75 @@ function initPositionBuffer(gl: WebGLRenderingContext) {
   return positionBuffer;
 }
 
+function initTextureBuffer(gl: WebGLRenderingContext) {
+  const textureCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+
+  const textureCoordinates = [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0];
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(textureCoordinates),
+    gl.STATIC_DRAW
+  );
+
+  return textureCoordBuffer;
+}
+
 function initBuffers(gl: WebGLRenderingContext) {
   const positionBuffer = initPositionBuffer(gl);
 
+  const textureCoordBuffer = initTextureBuffer(gl);
+
   return {
     position: positionBuffer,
+    textureCoord: textureCoordBuffer,
   };
+}
+
+function loadTexture(gl: WebGLRenderingContext, url: string) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([255, 255, 255, 255]);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    level,
+    internalFormat,
+    width,
+    height,
+    border,
+    srcFormat,
+    srcType,
+    pixel
+  );
+
+  const image = new Image();
+  image.onload = () => {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      level,
+      internalFormat,
+      srcFormat,
+      srcType,
+      image
+    );
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  };
+  image.src = url;
+
+  return texture;
 }
 
 function setPositionAttribute(gl: WebGLRenderingContext, buffers, programInfo) {
@@ -126,7 +204,25 @@ function setPositionAttribute(gl: WebGLRenderingContext, buffers, programInfo) {
   gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
 }
 
-function drawScene(gl: WebGLRenderingContext, programInfo, buffers) {
+function setTextureAttribute(gl: WebGLRenderingContext, buffers, programInfo) {
+  const num = 2;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+  gl.vertexAttribPointer(
+    programInfo.attribLocations.textureCoord,
+    num,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+  gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+}
+
+function drawScene(gl: WebGLRenderingContext, programInfo, buffers, texture) {
   gl.clearColor(0.364, 0.743, 0.385, 1.0);
   gl.clearDepth(1.0);
   gl.enable(gl.DEPTH_TEST);
@@ -155,6 +251,8 @@ function drawScene(gl: WebGLRenderingContext, programInfo, buffers) {
 
   setPositionAttribute(gl, buffers, programInfo);
 
+  setTextureAttribute(gl, buffers, programInfo);
+
   gl.useProgram(programInfo.program);
 
   gl.uniformMatrix4fv(
@@ -167,6 +265,12 @@ function drawScene(gl: WebGLRenderingContext, programInfo, buffers) {
     false,
     modelViewMatrix
   );
+
+  gl.activeTexture(gl.TEXTURE0);
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
   {
     const offset = 0;
